973cfc7375ec959ca28c89d73f6e83efe14486ea
Zorua <sanderb119@gmail.com>
2021-06-21 12:49:19 +0200

    buttons/logo on main page exist

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<h1>Fullstack StarTracker homepage</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="mainMenu.css">
+    <title>Star Game Tracker</title>
+</head>
+<body>
+<img id="pageLogo" src="img/StarGameTrackerLogo.png" alt="Star Tracker logo">
+
+<a href="#"><img src="img/MainMenuButton.png" alt="main menu button"></a>
+<a href="#"><img src="img/Register.png" alt="Register button"></a>
+<a href="#"><img src="img/LoginButton.png" alt="Login button"></a>
+<a href="#"><img src="img/ProfileButton.png" alt="Profile button"></a>
+<a href="#"><img src="img/LogoutButton.png" alt="Logout button"></a>
+
+<?php
+
+?>
+
+
+</body>
+</html>
