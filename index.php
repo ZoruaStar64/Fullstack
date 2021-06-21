eb96f119693315b88b3b8f8f74db9ef339c91bbe
Zorua <sanderb119@gmail.com>
2021-06-21 15:19:16 +0200

    got a favicon going and the page has been nicely styled so far (temporarily hid the logout and profile button with a boolean will implement the hiding of said buttons better when i'm able to get the account registering/logging in working)

--- index.php
+++ index.php
@@ -2,21 +2,29 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="mainMenu.css">
+    <link rel="shortcut icon" href="img/ColoredStar.png">
     <title>Star Game Tracker</title>
 </head>
 <body>
+<div class="pageContainer">
 <img id="pageLogo" src="img/StarGameTrackerLogo.png" alt="Star Tracker logo">
 
-<a href="#"><img src="img/MainMenuButton.png" alt="main menu button"></a>
-<a href="#"><img src="img/Register.png" alt="Register button"></a>
-<a href="#"><img src="img/LoginButton.png" alt="Login button"></a>
-<a href="#"><img src="img/ProfileButton.png" alt="Profile button"></a>
-<a href="#"><img src="img/LogoutButton.png" alt="Logout button"></a>
+
 
 <?php
+$showloggedin = false;
+echo "<div class='Shine'>";
+echo "<div><a href='#'><figure><img src='img/MainMenuButton.png' alt='main menu button'></figure></a></div>";
+echo "<div><a href='#'><figure><img src='img/Register.png' alt='Register button'></figure></a></div>";
+echo "<div><a href='#'><figure><img src='img/LoginButton.png' alt='Login button'></figure></a></div>";
+if ($showloggedin == true) {
+echo "<div><a href='#'><figure><img src='img/ProfileButton.png' alt='Profile button'></figure></a></div>";
+echo "<div><a href='#'><figure><img src='img/LogoutButton.png' alt='Logout button'></figure></a></div>";
+}
+echo "</div>"
 
 ?>
 
-
+</div>
 </body>
 </html>
