53735c79c3898f2edb7fc90404ed277cce730cee
ZoruaStar64 <sanderb119@gmail.com>
2021-09-16 10:22:43 +0200

    Terraspark checklist is 50% done just need php/database support

--- mainMenu.php
+++ mainMenu.php
@@ -25,8 +25,8 @@
 <div class="pageContainer">
 
     <div id="row1">
-        <img class="seperate" src="img/emptyTracker.png" width="125" height="175">
-        <img class="seperate" src="img/emptyTracker.png" width="125" height="175">
+        <a href="#"><img class="seperate" src="img/Terraria.png" width="125" height="175"></a>
+        <a href="#"><img class="seperate" src="img/luigi's%20mansion.png" width="125" height="175"></a>
         <img class="seperate" src="img/emptyTracker.png" width="125" height="175">
         <img class="seperate" src="img/emptyTracker.png" width="125" height="175">
         <img class="seperate" src="img/emptyTracker.png" width="125" height="175">
---
+++ terrariaChecklist.php
@@ -0,0 +1,82 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="CSS/terraria.css">
+    <link rel="shortcut icon" href="img/ColoredStar.png">
+    <title>Star Game Tracker</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Saira+Stencil+One&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div id="navbar">
+    <div class="Shine">
+        <div><a href='index.php'><figure><img class="linkButtons" src='img/HomeButton.png' alt='Home button'></figure></a></div>
+        <div><a href='profile.php'><figure><img class="linkButtons" src='img/ProfileButton.png' alt='main menu button'></figure></a></div>
+        <div><a href='index.php?logout'><figure><img class="linkButtons" src='img/LogoutButton.png' alt='Logout button'></figure></a></div>
+    </div>
+</div>
+
+<div class="pageContainer">
+
+
+    <div class="Terraspark">
+        <h1 id="terraSparkTitle">Terraspark</h1>
+
+        <div id="createFrostspark">
+            <p>
+                <img src="terrariaImg/Sailfish_Boots.png" alt="Sailfish Boots">
+                or
+                <img src="terrariaImg/Hermes_Boots.png" alt="Hermes Boots">
+                or
+                <img src="terrariaImg/Dunerider_Boots.png" alt="Dunerider Boots">
+                or
+                <img src="terrariaImg/Flurry_Boots.png" alt="Flurry Boots">
+                + <img src="terrariaImg/Rocket_Boots.png" alt="Rocket Boots">
+                <br> = <br>
+                 <img src="terrariaImg/Aglet.png" alt="Aglet">
+                + <img src="terrariaImg/Spectre_Boots.png" alt="Spectre Boots">
+                + <img src="terrariaImg/Anklet_of_the_Wind.png" alt="Anklet of the Wind">
+                <br> = <br>
+                <img src="terrariaImg/Lightning_Boots.png" alt="Lightning Boots">
+                + <img src="terrariaImg/Ice_Skates.png" alt="Ice Skates">
+                <br> = <br>
+                <img src="terrariaImg/Frostspark_Boots.png" alt="Frostspark Boots">
+            </p>
+        </div>
+
+        <div id="createTerraSpark">
+            <p>
+                <img src="terrariaImg/Frostspark_Boots.png" alt="Frostspark Boots">
+                +
+                <img src="terrariaImg/Lava_Waders.png" alt="Lava Waders">
+                <br> = <br>
+                <img src="terrariaImg/Terraspark_Boots.png" alt="Terraspark Boots">
+            </p>
+        </div>
+
+    <div id="createLavaWaders">
+        <p>
+        <img src="terrariaImg/Obsidian.png" alt="Obsidian"> x 20
+            <br> = <br>
+            <img src="terrariaImg/Obsidian_Skull.png" alt="Obsidian Skull">
+            + <img src="terrariaImg/Lava_Charm.png" alt="Lava Charm">
+            <br> = <br>
+            <img src="terrariaImg/Obsidian_Rose.png" alt="Obsidian Rose">
+            + <img src="terrariaImg/Molten_Charm.png" alt="Molten Charm">
+            + <img src="terrariaImg/Water_Walking_Boots.png" alt="Water Walking Boots">
+            <br> = <br>
+            <img src="terrariaImg/Lava_Waders.png" alt="Lava Waders">
+        </p>
+    </div>
+
+
+</div>
+</div>
+</body>
+</html>
